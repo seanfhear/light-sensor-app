a2f3276101ccc86ebadc127da9ba540ca9cb1ce3
seanfhear <hassetse@tcd.ie>
2020-11-22 19:44:04 +0000

    weather data persisting to db

--- src/app/src/main/java/com/seanh/lightsensor/MainActivity.kt
+++ src/app/src/main/java/com/seanh/lightsensor/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.*
 import com.google.android.gms.location.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -20,11 +22,17 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.seanh.lightsensor.models.Data
 import com.seanh.lightsensor.models.Reading
+import com.seanh.lightsensor.models.Weather
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 import java.time.LocalDateTime
 
 
 const val LightDeltaThresh = 1
+const val ReadingsDataRef = "readings"
+const val WeatherDataRef = "weather"
+
+val device = "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}"
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     private var sensorManager: SensorManager? = null
@@ -71,20 +79,28 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val data = ArrayList<Data>()
         val light = event.values[0]
         val time = LocalDateTime.now().toString()
-        val loc = currentLocation
-        val device = "${android.os.Build.MANUFACTURER} ${android.os.Build.MODEL}"
+
+        if (currentLocation == "") return
 
         data.add(Data("Light", "%.1f".format(light)))
         data.add(Data("Time", time))
-        data.add(Data("Location", loc))
+        data.add(Data("Location", currentLocation))
 
-        val ref: DatabaseReference = database.getReference("readings").push()
-        ref.setValue(Reading(
-            value = light.toString(),
-            time = time,
-            location = loc,
-            device = device
-        ))
+        val loc = currentLocation.split(",")
+        val lat = loc[0].toDouble()
+        val lon = loc[1].toDouble()
+
+        val ref: DatabaseReference = database.getReference(ReadingsDataRef).push()
+        ref.setValue(
+            Reading(
+                value = light.toString(),
+                time = time,
+                latitude = lat,
+                longitude = lon,
+                device = device
+            )
+        )
+        getCurrentWeather(lat, lon, time)
 
         recyclerView.adapter = DataAdapter(data)
     }
@@ -115,6 +131,34 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
         }
         LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(mLocationRequest, mLocationCallback, null)
+    }
+
+    private fun getCurrentWeather(lat: Double, lon: Double, time: String) {
+        val loc = currentLocation.split(",")
+        val weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=${loc[0]}&lon=${loc[1]}&exclude=hourly,daily,minutely,alerts&appid=bce4ba66924c5a700dddb9abb2e08382"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, weatherURL, null,
+            { response ->
+                val ref: DatabaseReference = database.getReference(WeatherDataRef).push()
+
+                val current: JSONObject = response.get("current") as JSONObject
+                ref.setValue(
+                    Weather(
+                        clouds = current.get("clouds") as Int,
+                        visibility = current.get("visibility") as Int,
+                        latitude = lat,
+                        longitude = lon,
+                        time = time
+                    )
+                )
+            },
+            {
+                // TODO: Handle error
+            }
+        )
+
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
 
     private fun onToggleOff() {
--- src/app/src/main/java/com/seanh/lightsensor/models/Reading.kt
+++ src/app/src/main/java/com/seanh/lightsensor/models/Reading.kt
@@ -3,6 +3,7 @@ package com.seanh.lightsensor.models
 data class Reading (
     var value: String,
     var time: String,
-    var location: String,
+    var latitude: Double,
+    var longitude: Double,
     var device: String
 )
